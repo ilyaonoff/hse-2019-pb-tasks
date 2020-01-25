8edde2cf0e973d4ac89c4ad959729ee94ad3cc03
Ilya Onofriychuk <ilyaonof@yandex.ru>
2020-01-25 09:18:12 +0300

    Add to upload task05

--- task03-bots/tictactoe_user_handler.py
+++ task03-bots/tictactoe_user_handler.py
@@ -1,6 +1,7 @@
 from typing import Callable, Optional
 from bot import UserHandler
 from tictactoe import Player, TicTacToe
+import sys
 
 
 class TicTacToeUserHandler(UserHandler):
@@ -17,37 +18,28 @@ class TicTacToeUserHandler(UserHandler):
         elif not self.game:
             self.send_message('Game is not started')
         else:
-            try:
-                character, input_col, input_row = message.split(maxsplit=2)
-                col, row = int(input_col), int(input_row)
-                player = self.char_to_player(character)
-                if not (player
-                        and 0 <= row < 3
-                        and 0 <= col < 3
-                        and self.game.can_make_turn(player, row=row, col=col)):
-                    self.send_message('Invalid turn')
-                    return
-                self.make_turn(Player(player), row=row, col=col)
-            except ValueError:
+            player, input_col, input_row = message.split(maxsplit=2)
+            col, row = int(input_col), int(input_row)
+            if not self.game.can_make_turn(Player[player], row=row, col=col):
                 self.send_message('Invalid turn')
-                return
+            else:
+                self.make_turn(Player[player], row=row, col=col)
 
     def start_game(self) -> None:
         """Начинает новую игру в крестики-нолики и сообщает об этом пользователю."""
         self.game = TicTacToe()
         self.send_field()
 
-    def char_to_player(self, char: str) -> Optional[Player]:
-        return {
-            'O': Player.O,
-            'X': Player.X
-        }.get(char, None)
-
     def finish_game(self) -> None:
         assert self.game
         winner = self.game.winner()
-        message_end = '{} wins'.format(winner.name) if winner else 'draw'
-        self.send_message('Game is finished, {}'.format(message_end))
+        if not winner:
+            message = 'Game is finished, draw'
+        elif winner == Player.X:
+            message = 'Game is finished, X wins'
+        else:
+            message = 'Game is finished, O wins'
+        self.send_message(message)
         self.game = None
 
     def make_turn(self, player: Player, *, row: int, col: int) -> None:
@@ -65,3 +57,7 @@ class TicTacToeUserHandler(UserHandler):
         for row in self.game.field:
             rows.append(''.join([cell.name if cell else '.' for cell in row]))
         self.send_message('\n'.join(rows))
+
+bot = TicTacToeUserHandler(send_message=print)
+for line in sys.stdin:
+    bot.handle_message(line.rstrip('\n'))
